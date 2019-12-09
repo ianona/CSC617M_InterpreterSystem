d45495ec614342f57b968877a74f9cc97b23dac9
wonsukCHO <36394364+wonsukCHO@users.noreply.github.com>
2019-12-09 20:48:31 +0800

    swd

--- src/sample/CodeOptimizer.java
+++ src/sample/CodeOptimizer.java
@@ -25,38 +25,44 @@ public class CodeOptimizer extends EzBrewBaseListener {
         String[] lines = wholecode.split(System.getProperty("line.separator"));
         StringBuffer sb = new StringBuffer();
         String[][] assignment = new String[99][4];
+        String codeline = "";
 
         for (int x = 0; x < lines.length; x++) {
             System.out.println("@@@@@@@@@@@@@@@@@@@");
             System.out.println(lines[x]);
             System.out.println("@@@@@@@@@@@@@@@@@@@");
 
-            lines[x] = lines[x].replaceAll("\\s", "");
+            codeline = lines[x].replaceAll("\\s", "");
 
-            if (lines[x].matches("[a-zA-Z]*[=][0-9]*[;]")) {
-                if (lines[x].contains(numerical[0]) || lines[x].contains(numerical[1]) || lines[x].contains(numerical[2])) {
-                    if (lines[x].contains(numerical[0])) {
+            if (codeline.matches("[a-zA-Z]*[=][0-9]*[;]")) {
+                if (codeline.contains(numerical[0]) || codeline.contains(numerical[1]) || codeline.contains(numerical[2])) {
+                    if (codeline.contains(numerical[0])) {
                         assignment[x][0] = numerical[0];
-                        lines[x] = lines[x].replaceAll(numerical[0], "");
-                        lines[x] = lines[x].replaceAll(";", "");
-                        assignment[x][1] = lines[x].substring(0, lines[x].indexOf("="));
-                        assignment[x][2] = lines[x].substring(lines[x].indexOf("=")+1);
-                        assignment[x][3] = Integer.toString(x+1);
-                    } else if (lines[x].contains(numerical[1])) {
+                        codeline = codeline.replaceAll(numerical[0], "");
+                        codeline = codeline.replaceAll(";", "");
+                        assignment[x][1] = codeline.substring(0, codeline.indexOf("="));
+                        assignment[x][2] = codeline.substring(codeline.indexOf("=") + 1);
+                        assignment[x][3] = Integer.toString(x);
+                    } else if (codeline.contains(numerical[1])) {
                         assignment[x][0] = numerical[1];
-                        lines[x] = lines[x].replaceAll(numerical[1], "");
-                        lines[x] = lines[x].replaceAll(";", "");
-                        assignment[x][1] = lines[x].substring(0, lines[x].indexOf("="));
-                        assignment[x][2] = lines[x].substring(lines[x].indexOf("=")+1);
-                        assignment[x][3] = Integer.toString(x+1);
-                    } else if (lines[x].contains(numerical[2])) {
+                        codeline = codeline.replaceAll(numerical[1], "");
+                        codeline = codeline.replaceAll(";", "");
+                        assignment[x][1] = codeline.substring(0, codeline.indexOf("="));
+                        assignment[x][2] = codeline.substring(codeline.indexOf("=") + 1);
+                        assignment[x][3] = Integer.toString(x);
+                    } else if (codeline.contains(numerical[2])) {
                         assignment[x][0] = numerical[2];
-                        lines[x] = lines[x].replaceAll(numerical[2], "");
-                        lines[x] = lines[x].replaceAll(";", "");
-                        assignment[x][1] = lines[x].substring(0, lines[x].indexOf("="));
-                        assignment[x][2] = lines[x].substring(lines[x].indexOf("=")+1);
-                        assignment[x][3] = Integer.toString(x+1);
+                        codeline = codeline.replaceAll(numerical[2], "");
+                        codeline = codeline.replaceAll(";", "");
+                        assignment[x][1] = codeline.substring(0, codeline.indexOf("="));
+                        assignment[x][2] = codeline.substring(codeline.indexOf("=") + 1);
+                        assignment[x][3] = Integer.toString(x);
                     }
+                }
+            } else {
+                if (codeline.contains("out")) {
+                    assignment[x][0] = "out";
+                    assignment[x][1] = Integer.toString(x);
                 }
             }
             System.out.println("---------------------------");
@@ -67,10 +73,39 @@ public class CodeOptimizer extends EzBrewBaseListener {
             System.out.println("---------------------------");
         }
 
+        for (int x = 0; x < lines.length; x++) {
+
+            for (int y = 0; y < lines.length; y++) {
+
+                if (assignment[y][0] != null) {
+
+                    if (lines[x].contains(assignment[y][1])) {
+                        lines[x] = lines[x].replaceAll(assignment[y][1], assignment[y][2]);
+                        lines[Integer.parseInt(assignment[y][3].trim())] = "";
+                    }
+                }
+            }
+        }
+
+        int x = 0;
+        while (!(lines[x].contains("out")) && x<lines.length) {
+            x++;
+        }
+
+        for (int y = x; y < lines.length; y++) {
+            System.out.println("@@@@---TESTING---@@@@");
+            System.out.println(lines[y]);
+            if (!(lines[y].contains("{") || lines[y].contains("}") || lines[y].contains("out"))) {
+                lines[y] = "";
+            }
+        }
+
 
         for (int i = 0; i < lines.length; i++) {
-            sb.append(lines[i]);
-            sb.append("\n");
+            if (lines[i] != "") {
+                sb.append(lines[i]);
+                sb.append("\n");
+            }
         }
 
         shortened = sb.toString();
